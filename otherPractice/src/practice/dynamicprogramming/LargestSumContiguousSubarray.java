0a2e26c220cf5e350e9a75e26e219a31c8a6e9ff
Vinod Akkepalli <shining.vinod@gmail.com>
2018-04-26 08:49:06 +0530

    Correction in program logic

--- otherPractice/src/practice/dynamicprogramming/LargestSumContiguousSubarray.java
+++ otherPractice/src/practice/dynamicprogramming/LargestSumContiguousSubarray.java
@@ -4,35 +4,41 @@ package practice.dynamicprogramming;
  * 
  * @author VinodAkkepalli
  *
- *	Problem Statement:
- *	Find a maximum sum in a largest contiguous subarray
- *	http://www.geeksforgeeks.org/largest-sum-contiguous-subarray/
- *	
- *	Solution:
- *	Follow dynamic programming approach to store previous max to compare with current
+ *         Problem Statement: Find a maximum sum in a largest contiguous
+ *         subarray
+ *         http://www.geeksforgeeks.org/largest-sum-contiguous-subarray/
+ * 
+ *         Solution: Follow dynamic programming approach to store previous max
+ *         to compare with current
  *
- *	TC: O(n)
+ *         TC: O(n)
  */
 
 public class LargestSumContiguousSubarray {
 
 	public static void main(String[] args) {
-		
-		int[] a =  {-2, -3, 4, -1, -2, 1, 5, -3};
-		
-		System.out.println("Maximum sum is: " + maxSubArraySum(a, a.length));
+
+		int[] a = { 10, -3, 4, -1, -2, 1, 5, -3 };
+		// int[] a = {-3, -2, 3, 10, -4, 7, 2, -5};
+
+		System.out.println("Maximum sum is: " + maxSubArraySum(a));
 	}
 
-	private static int maxSubArraySum(int[] a, int length) {
+	private static int maxSubArraySum(int[] a) {
 
-		int max_so_far = a[0];
-		int cur_max = a[0];
-		
-		for(int i = 0 ; i<length; i++){
-			cur_max = Math.max(a[i], cur_max+a[i]);
-			max_so_far = Math.max(max_so_far, cur_max);
+		// Dynamic Programming approach which takes up O(n) time complexity
+		int arrLength = a.length;
+		// variable to store maximum after including ith element
+		int currentMax = a[0];
+		// variable to store global maximum achieved after comparing with currentMax
+		int maxSoFar = a[0];
+
+		for (int i = 1; i < arrLength; i++) {
+			currentMax = Math.max(a[i], currentMax + a[i]);
+			maxSoFar = Math.max(maxSoFar, currentMax);
 		}
-		
-		return max_so_far;
+
+		return maxSoFar;
 	}
+
 }
