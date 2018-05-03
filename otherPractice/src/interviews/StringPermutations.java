cfd785902727d57a565b3fc317e8130ccccdb875
VinodAkkepalli <shining.vinod@gmail.com>
2018-05-03 19:27:39 +0530

    Print only distict permutations

--- otherPractice/src/interviews/StringPermutations.java
+++ otherPractice/src/interviews/StringPermutations.java
@@ -8,27 +8,65 @@ package interviews;
  *         Solution is based on backtracking and recursion
  *         https://www.youtube.com/watch?v=MQcwxQK2DPA&list=PL65BCEDD6788C3F27
  *         https://www.geeksforgeeks.org/write-a-c-program-to-print-all-permutations-of-a-given-string/
+ *         
+ *         Print only distinct permutations:
+ *         https://www.geeksforgeeks.org/distinct-permutations-string-set-2/
  */
 
 public class StringPermutations {
 
 	public static void main(String[] args) {
 		String str = "ABCD";
+		System.out.println("All possible permutations are: ");
 		printPermutations(str, 0, str.length() - 1);
+
+		String str1 = "ABCA";
+		System.out.println("Distict permutations are: ");
+		printDistinctPermutations(str1, 0, str1.length() - 1);
 	}
 
-	private static void printPermutations(String str, int l, int r) {
-		if (l == r) {
+	// TC O( n * n!)
+	private static void printPermutations(String str, int left, int right) {
+		if (left == right) {
 			System.out.println(str);
 			return;
 		}
 
-		for (int i = l; i <= r; i++) {
-			str = swap(str, l, i);
-			printPermutations(str, l + 1, r);
-			str = swap(str, l, i);
+		for (int i = left; i <= right; i++) {
+			str = swap(str, left, i);
+			printPermutations(str, left + 1, right);
+			str = swap(str, left, i);
+		}
+	}
+
+	// TC O( n * n!)
+	private static void printDistinctPermutations(String str, int left, int right) {
+		if (left >= right) {
+			System.out.println(str);
+			return;
 		}
 
+		for (int i = left; i <= right; i++) {
+
+			// Proceed further for str.charAt(i) only if it doesn't match with
+			// any of the characters after str.charAt(left)
+			if (shouldSwap(str, left, i, right)) {
+				str = swap(str, left, i);
+				printDistinctPermutations(str, left + 1, right);
+				str = swap(str, left, i);
+			}
+		}
+	}
+
+	// Returns true if str.charAt(curr) matches with any of the
+	// characters after str.charAt(start)
+	private static boolean shouldSwap(String str, int start, int curr, int n) {
+		for (int i = start; i < curr; i++) {
+			if (str.charAt(i) == str.charAt(curr)) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 	private static String swap(String str, int i, int j) {
@@ -39,5 +77,4 @@ public class StringPermutations {
 		charArray[j] = temp;
 		return String.valueOf(charArray);
 	}
-
 }
