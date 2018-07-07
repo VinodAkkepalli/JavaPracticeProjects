5a2f4fa56ab228e112e185fd235ceb969afbd8ae
Vinod Akkepalli <shining.vinod@gmail.com>
2018-07-07 23:21:52 +0530

    code for Hibernate save, update, saveOrUpdate and fetch

--- HibernateDemo/src/main/java/com/shine/hibernate/HibernateDemo/App.java
+++ HibernateDemo/src/main/java/com/shine/hibernate/HibernateDemo/App.java
@@ -11,10 +11,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        Student std1 = new Student();
-        std1.setName("Vinod");
-        std1.setRollNumber(1);
-        
         //Configurations
         File hibernatePropFile = new File("./src/main/resources/hibernate.cfg.xml");
         Configuration configuration = new Configuration().
@@ -25,9 +21,49 @@ public class App
         SessionFactory sessionFactory = configuration.buildSessionFactory();
         Session session = sessionFactory.openSession();
         
-        // Transaction and commit
+//        insertData(session);
+        updateData(session);
+        saveOrUpdateData(session);
+        fetchData(session);
+    }
+    
+    static void fetchData(Session session) {
+    	Transaction transaction = session.beginTransaction();
+        Student std = (Student)session.get(Student.class, 2);
+        System.out.println("Student record is: " + std);
+        transaction.commit();    	
+    }
+    
+    static void insertData(Session session) {
+    	Student std1 = new Student();
+        std1.setName("Rajesh");
+        std1.setRollNumber(10);
+        
         Transaction transaction = session.beginTransaction();
+        // save for saving a record
         session.save(std1);
+        transaction.commit();
+    }
+    
+    static void updateData(Session session) {
+    	Student std1 = new Student();
+        std1.setName("Vijay");
+        std1.setRollNumber(6);
+        
+        Transaction transaction = session.beginTransaction();
+        // update for updating a record
+        session.update(std1);
+        transaction.commit();
+    }
+    
+    static void saveOrUpdateData(Session session) {
+    	Student std1 = new Student();
+        std1.setName("Raghu");
+        std1.setRollNumber(1);
+        
+        Transaction transaction = session.beginTransaction();
+        // saveOrUpdate saves a new record with new primary key or updates existing data
+        session.saveOrUpdate(std1);
         transaction.commit();
     }
 }
--- HibernateDemo/src/main/java/com/shine/hibernate/HibernateDemo/Student.java
+++ HibernateDemo/src/main/java/com/shine/hibernate/HibernateDemo/Student.java
@@ -1,14 +1,16 @@
 package com.shine.hibernate.HibernateDemo;
 
 import javax.persistence.Id;
-
+import javax.persistence.Column;
 import javax.persistence.Entity;
 
 @Entity
 public class Student {
 
 	@Id
+	@Column(name="roll_number")
 	private int rollNumber;
+	@Column(name="student_name")
 	private String name;
 
 	public String getName() {
@@ -26,4 +28,11 @@ public class Student {
 	public void setRollNumber(int rollNumber) {
 		this.rollNumber = rollNumber;
 	}
+
+	@Override
+	public String toString() {
+		return "Student [rollNumber=" + rollNumber + ", name=" + name + "]";
+	}
+	
+	
 }
