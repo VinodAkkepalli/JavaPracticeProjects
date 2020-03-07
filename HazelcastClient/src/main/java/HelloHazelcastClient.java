57236775976cb1f9ac431c6ed04de77bdf89bf06
Vinod Akkepalli <shining.vinod@gmail.com>
2020-03-07 23:19:41 +0530

    Hazelcast client app
    Hazelcast client app setup

---
+++ HazelcastClient/src/main/java/HelloHazelcastClient.java
@@ -0,0 +1,16 @@
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IMap;
+
+public class HelloHazelcastClient {
+
+    public static void main(String[] args) {
+        ClientConfig clientConfig = new ClientConfig();
+        clientConfig.addAddress("127.0.0.1:5701");
+        HazelcastInstance client = HazelcastClient.newHazelcastClient(clientConfig);
+        IMap map = client.getMap("rankings");
+        System.out.println("Vinod : " + map.get("Vinod"));
+        System.out.println(map.entrySet());
+    }
+}
