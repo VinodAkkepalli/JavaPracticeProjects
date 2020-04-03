4d9f578852b78e1f8116fed7a6dd387c8446b5a9
Vinod Akkepalli <shining.vinod@gmail.com>
2020-04-03 08:39:38 +0530

    InterruptedException code example

---
+++ BruceEckel/src/practice/concurrency/synchronization/InterruptedExceptionDemo.java
@@ -0,0 +1,38 @@
+package practice.concurrency.synchronization;
+
+/**
+ * <b>Description</b> :
+ *
+ * @author Vinod Akkepalli
+ */
+public class InterruptedExceptionDemo implements Runnable {
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(4000);
+            if(Thread.interrupted()){
+                System.out.println("I have been interrupted!");
+            }
+        } catch (InterruptedException e) {
+            //To preserve the interruption; observe isInterrupted() value on the next line
+            Thread.currentThread().interrupt();
+            System.out.println(Thread.currentThread().getName() +
+                    " Inside catch block:: isInterrupted() value: " + Thread.currentThread().isInterrupted());
+            e.printStackTrace();
+            return;
+        }
+        System.out.println(Thread.currentThread().getName() + " is Running");
+    }
+
+    public static void main(String[] args) {
+        Thread t0= new Thread(new InterruptedExceptionDemo());
+        t0.start();
+        System.out.println(t0.getName() + " isInterrupted? : " + t0.isInterrupted());
+
+        Thread t1= new Thread(new InterruptedExceptionDemo());
+        t1.start();
+        t1.interrupt();
+        System.out.println(t1.getName() + " isInterrupted? : " + t1.isInterrupted());
+    }
+}
