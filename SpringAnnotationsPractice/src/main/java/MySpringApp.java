1db566135f3c642f4e667dd368c5fcfdc6a6136e
Vinod Akkepalli <shining.vinod@gmail.com>
2020-06-27 22:03:31 +0530

    PreDestroy PostCreate annotations; Project Lombok

--- SpringAnnotationsPractice/src/main/java/MySpringApp.java
+++ SpringAnnotationsPractice/src/main/java/MySpringApp.java
@@ -1,6 +1,7 @@
 import com.practice.spring.shapes.Circle;
+import com.practice.spring.shapes.LineSegment;
 import com.practice.spring.shapes.Point;
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
@@ -10,12 +11,16 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class MySpringApp {
     public static void main(String[] args) {
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
+        AbstractApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
+        applicationContext.registerShutdownHook();
 
         Point point = (Point) applicationContext.getBean("zeroPoint");
         System.out.println(point.toString());
 
         Circle circle = (Circle) applicationContext.getBean("circle");
         System.out.println(circle.toString());
+
+        LineSegment lineSegment = (LineSegment) applicationContext.getBean("lineSegment");
+        System.out.println(lineSegment.toString());
     }
 }
--- SpringAnnotationsPractice/src/main/java/com/practice/spring/shapes/Point.java
+++ SpringAnnotationsPractice/src/main/java/com/practice/spring/shapes/Point.java
@@ -1,42 +1,21 @@
 package com.practice.spring.shapes;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
 /**
  * <b>Description</b> : Point class representing a point on a plane
+ * Data annotation of Project Lombok is used to avoid boilerplate code
  *
  * @author Vinod Akkepalli
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Component
 public class Point {
     private float x;
     private float y;
-
-    public Point(){}
-
-    public Point(float x, float y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public float getX() {
-        return x;
-    }
-
-    public void setX(float x) {
-        this.x = x;
-    }
-
-    public float getY() {
-        return y;
-    }
-
-    public void setY(float y) {
-        this.y = y;
-    }
-
-    @Override
-    public String toString() {
-        return "Point{" +
-                "x=" + x +
-                ", y=" + y +
-                '}';
-    }
 }
